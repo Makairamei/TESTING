f97092c7f0dd8612231edf28a5c7da6e40f297b2
sad25kag <sad25kag@gmail.com>
2026-05-12 20:19:15 +0700

    Update build.gradle.kts

--- YunshanID/build.gradle.kts
+++ YunshanID/build.gradle.kts
@@ -1,16 +1,28 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.gradle.api.Project
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
 version = 1
 
 cloudstream {
-    language = "id"
-    description = "Donghua & Anime provider dari YunshanID"
-    authors = listOf("Betbet")
-
-    status = 1
+    mainClass = "com.betbet.yunshanid.YunshanIDPlugin" 
+    
+    name = "YunshanID"
+    [span_1](start_span)description = "Donghua & Anime provider dari YunshanID"[span_1](end_span)
+    [span_2](start_span)authors = listOf("Betbet")[span_2](end_span)
+    [span_3](start_span)language = "id"[span_3](end_span)
+    
+    [span_4](start_span)status = 1[span_4](end_span)
 
     tvTypes = listOf(
         "Anime",
         "TvSeries"
-    )
+    [span_5](start_span))
 
-    iconUrl = "https://yunshanid.site/favicon.ico"
+    iconUrl = "https://yunshanid.site/favicon.ico"[span_5](end_span)
 }
