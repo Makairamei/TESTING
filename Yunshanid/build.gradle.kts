69387688081629372c72f8f71eb0c5e26cc39d49
sad25kag <sad25kag@gmail.com>
2026-05-13 12:33:22 +0700

    Update build.gradle.kts

--- Yunshanid/build.gradle.kts
+++ Yunshanid/build.gradle.kts
@@ -1,19 +1,15 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
-apply(plugin = "com.android.library")
-apply(plugin = "kotlin-android")
-apply(plugin = "com.github.recloudstream")
+version = 1
 
 cloudstream {
-    // Properti ini adalah yang paling aman dan tidak akan bentrok dengan Gradle
-    pluginId = "Yunshanid"
-    pluginName = "Yunshanid"
-    pluginClass = "com.Yunshanid.YunshanidPlugin"
-    description = "Dibuat oleh BetbetMiro untuk Yunshanid"
+    language = "id"
+    description = "Yunshanid — Streaming Movie, TV Series dan Anime Sub Indo"
     authors = listOf("BetbetMiro")
-}
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+    )
 
-dependencies {
-    val cloudstreamVersion = "latest-SNAPSHOT"
-    compileOnly("com.github.recloudstream:cloudstream3:$cloudstreamVersion")
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://yunshanid.site&size=%size%"
 }
