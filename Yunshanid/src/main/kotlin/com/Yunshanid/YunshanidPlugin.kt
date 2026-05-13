077a851edfbfae9f5c45c7c37e209a3d9ea6ac09
sad25kag <sad25kag@gmail.com>
2026-05-13 12:34:09 +0700

    Update YunshanidPlugin.kt

--- Yunshanid/src/main/kotlin/com/Yunshanid/YunshanidPlugin.kt
+++ Yunshanid/src/main/kotlin/com/Yunshanid/YunshanidPlugin.kt
@@ -1,13 +1,22 @@
-package com.Yunshanid
+package Yunshanid
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.extractors.*
 import android.content.Context
 
 @CloudstreamPlugin
-class YunshanidPlugin: Plugin() {
+class YunshanidPlugin : Plugin() {
     override fun load(context: Context) {
-        // Mendaftarkan API utama
         registerMainAPI(YunshanidProvider())
+        
+        // Mendaftarkan Extractor agar link video bisa langsung diputar
+        registerExtractorAPI(PixelDrain())
+        registerExtractorAPI(Gofile())
+        registerExtractorAPI(Krakenfiles())
+        registerExtractorAPI(Mediafire())
+        registerExtractorAPI(Mp4Upload())
+        registerExtractorAPI(FileMoon())
+        registerExtractorAPI(StreamWish())
     }
 }
