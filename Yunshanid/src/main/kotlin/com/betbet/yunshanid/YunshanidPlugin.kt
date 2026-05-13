91b726d0fb2efb9af704978e91ef22355c6526b7
sad25kag <sad25kag@gmail.com>
2026-05-13 17:41:02 +0700

    Create YunshanidPlugin.kt

---
+++ Yunshanid/src/main/kotlin/com/betbet/yunshanid/YunshanidPlugin.kt
@@ -0,0 +1,11 @@
+package com.betbet.yunshanid
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class YunshanidPlugin: Plugin() {
+    override fun load() {
+        registerMainAPI(YunshanidProvider())
+    }
+}
