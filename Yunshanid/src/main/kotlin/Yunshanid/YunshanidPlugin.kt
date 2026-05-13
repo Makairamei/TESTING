a1a0f5b000dc15a75ae9ed31022a0ac439f0fce0
sad25kag <sad25kag@gmail.com>
2026-05-13 20:30:37 +0700

    Update YunshanidPlugin.kt

--- Yunshanid/src/main/kotlin/Yunshanid/YunshanidPlugin.kt
+++ Yunshanid/src/main/kotlin/Yunshanid/YunshanidPlugin.kt
@@ -2,14 +2,12 @@ package Yunshanid
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context // Wajib diimport
 
 @CloudstreamPlugin
 class YunshanidPlugin : Plugin() {
-
-    override fun load() {
-
-        registerMainAPI(
-            YunshanidProvider()
-        )
+    // WAJIB ada parameter (context: Context) agar aplikasi bisa nge-load
+    override fun load(context: Context) {
+        registerMainAPI(YunshanidProvider())
     }
 }
