ca2e5cdfac42d88119282b91cb18d88a8728069d
sad25kag <sad25kag@gmail.com>
2026-05-19 22:09:19 +0700

    fix(pusatfilm): disable cross-platform to allow android imports

--- Pusatfilm/build.gradle.kts
+++ Pusatfilm/build.gradle.kts
@@ -4,12 +4,12 @@ cloudstream {
     description = "PusatFilm - Streaming Movie and TV Series"
     language = "id"
     authors = listOf("BetbetMiro") 
-    isCrossPlatform = true
+    // isCrossPlatform = true  <-- HAPUS ATAU KOMENTARI BARIS INI
+    isCrossPlatform = false 
     status = 1
     tvTypes = listOf(
-        "Movie",
+        "AsianDrama",
         "TvSeries",
-        "Anime",
-        "AsianDrama"
+        "Movie"
     )
 }
